d0da5dcc31087abedbbe1164ba58d9e59b7012bc
mjuned <mmj.nowaz@gmail.com>
2022-05-31 05:07:42 +0000

    fixed syntax error on head.js

--- head.js
+++ head.js
@@ -11,5 +11,5 @@ const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5, 6, 7], 5));
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

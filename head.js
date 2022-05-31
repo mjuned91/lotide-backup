a98ff18194347132de90602ad01da15573f980f3
mjuned <mmj.nowaz@gmail.com>
2022-05-31 02:55:33 +0000

    tail.js

--- head.js
+++ head.js
@@ -6,10 +6,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function(actual, expected) {
-  console.log(actual[0])
-  return actual[0]
-}
+const head = function(arr) {
+  console.log(arr[0]);
+  return arr[0];
+};
 
 assertEqual(head([5, 6, 7], 5));
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

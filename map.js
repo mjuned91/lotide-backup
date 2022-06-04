e223c9d7899c7e54ed0c5c047e92d886fd41ee3d
mjuned <mmj.nowaz@gmail.com>
2022-06-04 02:10:45 +0000

    map.js

--- map.js
+++ map.js
@@ -1,3 +1,6 @@
+//Built-in function which creates a new array populated with the results
+// of calling a provided function on every element in the calling array.
+
 const map = (array, callback) => {
   const results = [];
   for (let item of array) {

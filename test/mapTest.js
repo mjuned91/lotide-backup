4ceab12a544e5913f5a606f8e6fd018f034408c8
mjuned <mmj.nowaz@gmail.com>
2022-06-13 17:33:33 +0000

    Added Mocha and Chai tests to the file

--- test/mapTest.js
+++ test/mapTest.js
@@ -2,17 +2,18 @@ const assert = require('chai').assert;
 const map = require('../map');
 
 describe("#map", () => {
-  const words1 = ["ground", "control", "to", "major", "tom"];
   it("should return [ 'g', 'c', 't', 'm', 't' ] for (word => word[0])", () => {
+    const words1 = ["ground", "control", "to", "major", "tom"];
     assert.deepEqual(map(words1, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
   });
 
   it("should return [ 'r', 'o', 'o', 'a', 'o' ] for (word => word[1])", () => {
+    const words1 = ["ground", "control", "to", "major", "tom"];
     assert.deepEqual(map(words1, word => word[1]), [ 'r', 'o', 'o', 'a', 'o' ]);
   });
 
-  const numbers = [1, 2, 3, 4, 5];
   it("should return [ 3, 4, 5, 6, 7 ] for (numbers, num => num + 2)", () => {
+    const numbers = [1, 2, 3, 4, 5];
     assert.deepEqual(map(numbers, num => num + 2), [ 3, 4, 5, 6, 7 ]);
   });
 });

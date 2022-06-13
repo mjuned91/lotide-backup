060dc09ec1512500d29c878bed5a8a5957b22062
mjuned <mmj.nowaz@gmail.com>
2022-06-13 15:50:48 +0000

    Refactored so the code can be exported

--- flatten.js
+++ flatten.js
@@ -9,37 +9,7 @@ const flatten = (arr) => {
       flat.push(arr[i]);
     }
   }
-  console.log(flat);
+  return flat;
 };
 
-flatten([1, 2, [3, 4], 5, [6]]);
-
-
-/*
-const assertArraysEqual = (arr1, arr2) => {
-
-  const eqArrays = (arr1, arr2) => {
-    if (arr1.length !== arr2.length) {
-      console.log(false);
-      return;
-    }
-    
-    let ans = true;
-    for (let i = 0; i < (arr1.length); i++) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(false);
-        return;
-      }
-    }
-    console.log(ans);
-    return ans;
-  };
-
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  };
-}
-
-*/
+module.exports = flatten;

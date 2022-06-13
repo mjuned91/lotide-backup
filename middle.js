c198bfe453142b5b2b6921687005f8357b1a18af
mjuned <mmj.nowaz@gmail.com>
2022-06-13 08:37:27 +0000

    Added module.exports

--- middle.js
+++ middle.js
@@ -1,17 +1,17 @@
 const middle = (arr) => {
   let newArr = [];
-  if (arr.length < 3) {
-    console.log(newArr);
+  if (arr.length <= 2) {
+    return newArr;
   } else if (arr.length % 2 === 0) {
     let i = (arr.length / 2) - 1;
     let j = (arr.length / 2);
     newArr = [arr[i], arr[j]];
-    console.log(newArr);
+    return newArr;
   } else {
     let i = ((arr.length - 1) / 2);
     newArr = [arr[i]];
-    console.log(newArr);
+    return newArr;
   }
 };
 
-module.exports = head;
+module.exports = middle;

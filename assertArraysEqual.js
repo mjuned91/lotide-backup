42b8ac12764751e28d5de7c7d82451d52d754592
mjuned <mmj.nowaz@gmail.com>
2022-06-13 05:48:46 +0000

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,13 @@
-const assertArraysEqual = (arr1, arr2) => {
+const eqArrays = require('./eqArrays');
 
+const assertArraysEqual = (arr1, arr2) => {
+/*
   const eqArrays = (arr1, arr2) => {
     if (arr1.length !== arr2.length) {
       console.log(false);
       return;
     }
-    
+
     let ans = true;
     for (let i = 0; i < (arr1.length); i++) {
       if (arr1[i] !== arr2[i]) {
@@ -16,7 +18,7 @@ const assertArraysEqual = (arr1, arr2) => {
     console.log(ans);
     return ans;
   };
-
+*/
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
     } else {
@@ -25,3 +27,5 @@ const assertArraysEqual = (arr1, arr2) => {
 }
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+module.exports = assertArraysEqual;

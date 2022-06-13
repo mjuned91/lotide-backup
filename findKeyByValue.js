ba513ef5a064548d9e803043ce7ebb762ff69e11
mjuned <mmj.nowaz@gmail.com>
2022-06-13 15:41:18 +0000

    Refactored so the code can be exported

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,17 +1,10 @@
-//Helps us search for a key on an object where its value matches a given value.
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
 const findKeyByValue = (object, value) => {
   let keyPack = Object.keys(object);
   for (let key of keyPack) {
     if (object[key] === value) {
-      console.log(object[key]);
       return key;
-      }
     }
+  }
 };
+
+module.exports = findKeyByValue;

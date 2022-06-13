b86607af667e14d1ff927c3fe28456e1361904ab
mjuned <mmj.nowaz@gmail.com>
2022-06-13 15:03:43 +0000

    Added Mocha and Chai tests to the file

---
+++ test/countOnlyTest.js
@@ -0,0 +1,30 @@
+const { assert } = require("chai");
+const countOnly = require("../countOnly");
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+describe("#tail", () => {
+  it("should return 1 for result1['Jason']", () => {
+    assert.strictEqual(result1["Jason"], 1);
+  });
+
+  it("should return undefined for result1['Karima']", () => {
+    assert.strictEqual(result1["Karima"], undefined);
+  });
+
+  it("should return { Fang: 2, Jason: 1 }", () => {
+    assert.deepEqual(result1, { Fang: 2, Jason: 1 });
+  });
+});

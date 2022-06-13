91fe19e7f3e81f05472d47435be4530aedfd4405
mjuned <mmj.nowaz@gmail.com>
2022-06-13 16:34:56 +0000

    Refactored so the code can be exported

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,3 @@
-//This function will return a "slice of the array with elements taken from the beginning".
-//It should keep going until the callback returns a truthy value.
-
 const takeUntil = (array, callback) => {
   const results = [];
   for (let i = 0; i < array.length; i++) {
@@ -27,12 +24,4 @@ const takeUntil = function(array, callback) {
 };
 */
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+module.exports = takeUntil;

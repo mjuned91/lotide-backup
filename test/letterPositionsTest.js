b70458e09e38869d6023e1c76168245750f8d15a
mjuned <mmj.nowaz@gmail.com>
2022-06-13 17:32:29 +0000

    Added Mocha and Chai tests to the file

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -2,13 +2,13 @@ const { assert } = require("chai");
 const letterPositions = require("../letterPositions");
 
 describe('#letterPositions', () => {
-  const results1 = letterPositions("hello");
   it("should return [0] for results1['h']", () => {
+    const results1 = letterPositions("hello");
     assert.deepEqual(results1['h'], [0]); 
   });
 
-  const results2 = letterPositions("lighthouse in the house");
   it("should return [3, 5, 15, 18] for results2['h']", () => {
+    const results2 = letterPositions("lighthouse in the house");
     assert.deepEqual(results2['h'], [3, 5, 15, 18]);
   });
 });

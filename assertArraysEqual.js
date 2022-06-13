f7a9f01de792df33765a576920cf83d3b670432a
mjuned <mmj.nowaz@gmail.com>
2022-06-13 17:53:52 +0000

    Refactored so the code can be exported

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,8 +3,10 @@ const eqArrays = require('./eqArrays');
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    return true;
     } else {
       console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+      return false;
   };
 }
 

843f5a6c8ed79471d7c72630634f00ef1909958d
mjuned <mmj.nowaz@gmail.com>
2022-06-13 08:40:53 +0000

    Refactored the test files using Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,2 +1,16 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+
+describe("#middle", () => {
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("should return [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("should return [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});

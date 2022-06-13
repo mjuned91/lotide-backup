87f937924121000555b94c84f027012d03cc294a
mjuned <mmj.nowaz@gmail.com>
2022-06-13 17:30:35 +0000

    Added Mocha and Chai tests to the file

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -3,25 +3,27 @@ const eqObjects = require('../eqObjects');
 
 
 describe("eqObjects", () => {
-  const ab = { a: "1", b: "2" };
-  const ba = { b: "2", a: "1" };
-  const abc = { a: "1", b: "2", c: "3" };
   it("should return true for eqObjects(ab, ba)", () => {
+    const ab = { a: "1", b: "2" };
+    const ba = { b: "2", a: "1" };
     assert.strictEqual(eqObjects(ab, ba), true);
   });
 
   it("should return false for eqObjects(ab, abc)", () => {
+    const ab = { a: "1", b: "2" };
+    const abc = { a: "1", b: "2", c: "3" };
     assert.strictEqual(eqObjects(ab, abc), false);
   });
 
-  const cd = { c: "1", d: ["2", 3] };
-  const dc = { d: ["2", 3], c: "1" };
-  const cd2 = { c: "1", d: ["2", 3, 4] };
   it("should return true for eqObjects(cd, dc)", () => {
+    const cd = { c: "1", d: ["2", 3] };
+    const dc = { d: ["2", 3], c: "1" };
     assert.strictEqual(eqObjects(cd, dc), true);
   });
 
   it("should return false for eqObjects(cd, cd2)", () => {
+    const cd = { c: "1", d: ["2", 3] };
+    const cd2 = { c: "1", d: ["2", 3, 4] };
     assert.deepEqual(eqObjects(cd, cd2), false);
   });
 });

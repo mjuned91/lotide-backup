f19a9fd45f9a5d341e4a080378a614806142dcaa
mjuned <mmj.nowaz@gmail.com>
2022-06-13 08:41:11 +0000

    Refactored the test files using Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,25 +2,25 @@ const assert = require('chai').assert;
 const tail = require('../tail');
 
 describe("#tail", () => {
-  it("returns 2 for result.length", () => {
+  it("should return 2 for result.length", () => {
     const result = ["Hello", "Lighthouse", "Labs"];
 
     assert.strictEqual(tail(result).length, 2);
   });
 
-  it("returns 'Lighthouse' for result[0]", () => {
+  it("should return 'Lighthouse' for result[0]", () => {
     const result = ["Hello", "Lighthouse", "Labs"];
 
     assert.strictEqual(tail(result)[0], "Lighthouse");
   });
 
-  it("returns 'Labs' for result[1]", () => {
+  it("should return 'Labs' for result[1]", () => {
     const result = ["Hello", "Lighthouse", "Labs"];
 
     assert.strictEqual(tail(result)[1], "Labs");
   });
 
-  it("returns ['Lighthouse', 'Labs'] for tail(words)", () => {
+  it("should return ['Lighthouse', 'Labs'] for tail(words)", () => {
     const words = ["Yo Yo", "Lighthouse", "Labs"];
 
     assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);

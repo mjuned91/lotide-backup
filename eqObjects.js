7a3c4a1324cee585ed7616bffffa9764bddb707a
mjuned <mmj.nowaz@gmail.com>
2022-06-13 15:27:32 +0000

    Refactored so the code can be exported

--- eqObjects.js
+++ eqObjects.js
@@ -1,6 +1,3 @@
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-
 const eqObjects = function(object1, object2) {
   let objLength1 = Object.keys(object1).length
   let objLength2 = Object.keys(object2).length
@@ -34,17 +31,3 @@ const eqObjects = function(object1, object2) {
 };
 
 module.exports = eqObjects;
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false

be28403545313a75400f587d8994ce199a920df8
mjuned <mmj.nowaz@gmail.com>
2022-06-13 17:55:30 +0000

    Refactored so the code can be exported

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -4,8 +4,10 @@ const assertObjectsEqual = function(actual, expected) {
   const inspect = require("util").inspect;
   if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    return true;
     } else {
       console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+      return false;
   };
 };
 

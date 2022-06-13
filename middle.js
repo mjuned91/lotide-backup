02ff1233ef33cb97a46fe4a33f930a4f82ac4fad
mjuned <mmj.nowaz@gmail.com>
2022-06-13 06:22:42 +0000

    middle.js

--- middle.js
+++ middle.js
@@ -14,30 +14,4 @@ const middle = (arr) => {
   }
 };
 
-/*
-const assertArraysEqual = (arr1, arr2) => {
-
-  const eqArrays = (arr1, arr2) => {
-    if (arr1.length !== arr2.length) {
-      console.log(false);
-      return;
-    }
-    
-    let ans = true;
-    for (let i = 0; i < (arr1.length); i++) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(false);
-        return;
-      }
-    }
-    console.log(ans);
-    return ans;
-  };
-
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  };
-}
-*/
+module.exports = head;
